ca8563ae42865ec321a7f5284841e63262e5881d
“Akky830” <ryoutokurpg@gmail.com>
2021-02-07 12:16:51 +0900

    一回目

---
+++ ex4/ex4.js
@@ -0,0 +1,51 @@
+
+// ex04-1.js //
+
+function cointoss() {
+  const num = Math.random()
+  if(num >=0.5) {
+    console.log('表')
+  }　else {
+    console.log('裏')
+  } 
+};
+for (let i = 0; i < 10; i++) {
+  cointoss()
+}
+
+// ex04-2.js //
+
+function scoring(score) {
+  if(score >= 90) {
+    console.log('秀')
+  }  else if(score >= 60){
+    console.log('良')
+  }　else if (score >= 30) {
+    console.log('可')
+  } else {
+    console.log('不可')
+  }
+}
+
+console.log(scoring(100)) //=> 秀
+console.log(scoring(60)) //=> 良
+console.log(scoring(13)) //=> 不可
+
+// ex04-3.js //
+
+const scores = [13, 67, 18, 54, 30, 22, 89, 49, 21, 65]
+
+function scorenig(score) {
+  if(score >= 90) {
+    console.log('秀')
+  }  else if(score >= 60){
+    console.log('良')
+  }　else if (score >= 30) {
+    console.log('可')
+  } else {
+    console.log('不可')
+  }
+}
+scores.forEach((scorenig) => {
+  console.log(scorenig)
+})
