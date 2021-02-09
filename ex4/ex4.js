101b70a79ff2e652ed199c6858d7bc7d06ea972a
“Akky830” <ryoutokurpg@gmail.com>
2021-02-09 16:27:30 +0900

    コミット3回目

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -3,10 +3,10 @@
 
 function cointoss() {
   const num = Math.random()
-  if(num >=0.5) {
-    console.log('表')
+  if(num >= 0.5) {
+    console.log('表');
   }　else {
-    console.log('裏')
+    console.log('裏');
   } 
 };
 for (let i = 0; i < 10; i++) {
@@ -15,37 +15,39 @@ for (let i = 0; i < 10; i++) {
 
 // ex04-2.js //
 
-function scoring(score) {
+const score = Math.random() * 100
+
+function scoreing(score) {
   if(score >= 90) {
-    console.log('秀')
+    console.log('秀');
   }  else if(score >= 60 && score < 90) {
-    console.log('良')
+    console.log('良');
   }　else if (score >= 30 && score < 60) {
-    console.log('可')
+    console.log('可');
   } else {
-    console.log('不可')
+    console.log('不可');
   }
 }
 
-console.log(scoring(100)) //=> 秀
-console.log(scoring(60)) //=> 良
-console.log(scoring(13)) //=> 不可
+console.log(scoreing(100));//=> 秀
+console.log(scoreing(60)); //=> 良
+console.log(scoreing(13)); //=> 不可
 
 // ex04-3.js //
 
 const scores = [13, 67, 18, 54, 30, 22, 89, 49, 21, 65]
 
-function scorenig(score) {
+function scoreing(score) {
   if(score >= 90) {
-    console.log('秀')
-  }  else if(score >= 60){
-    console.log('良')
-  }　else if (score >= 30) {
-    console.log('可')
+    console.log('秀');
+  }  else if(score >= 60 && score < 90) {
+    console.log('良');
+  }　else if (score >= 30 && score < 60) {
+    console.log('可');
   } else {
-    console.log('不可')
+    console.log('不可');
   }
 }
 scores.forEach((score) => {
-  console.log(score,scorenig(score))
+  console.log(score,scoreing(score));
 })
