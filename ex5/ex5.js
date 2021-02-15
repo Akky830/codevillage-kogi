28733e04b71db11b8771fc010dd2852d11775841
“Akky830” <ryoutokurpg@gmail.com>
2021-02-15 10:59:28 +0900

    ex5

--- ex5/ex5.js
+++ ex5/ex5.js
@@ -38,4 +38,4 @@ console.log(obj.items[0].name) //=> Item 123
 console.log(obj.items[0].id) //=> 123
 console.log(obj.items[1].name) //=> Item 456
 console.log(obj.items[1].id) //=> 456
-console.log(obj.next.id) //=> 789g
+console.log(obj.next.id) //=> 789
